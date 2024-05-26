91160fb29d3bbf2c5696fb36411e2fa168bf4438
Ayush Mishra <am2014@srmist.edu.in>
2024-05-26 18:00:03 +0530

    chore: update hero image (finally!)

--- components/hero.js
+++ components/hero.js
@@ -1,63 +1,40 @@
-import React, { useState, useEffect, useCallback } from "react";
+import Image from "next/image";
+import React, { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import Image from "next/image";
-import Link from "next/link";
 
 function Hero() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
   }, []);
 
   return (
-    <div className="flex justify-center">
+    <div className=" flex justify-center items-center h-screen">
       <Particles
         id="tsparticles"
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-screen h-full"
         url="particles.json"
         init={particlesInit}
       />
-      <div className="relative h-screen flex flex-col items-center justify-center">
-        <div className="relative w-1/3">
+      <div className="relative flex flex-col items-center justify-center h-full">
+        <div className="relative w-72">
           <Image
-            src="/ayush.jpg"
-            alt="Ayush"
-            width="100%"
-            height="100%"
+            src="/Ayush Mishra.png"
+            alt="Ayush Mishra"
+            width={400}
+            height={400}
             layout="responsive"
             objectFit="contain"
             className="rounded-full"
             priority={true}
           />
         </div>
-
         <div className="p-10 text-center">
-          <h1 className="font-silkscreen text-6xl">Ayush Mishra</h1>
+          <h1 className="font-silkscreen text-2xl md:text-4xl">Ayush Mishra</h1>
           <h2 className="font-mono p-5">
             Generalist Developer - Fueled by Curiosity
           </h2>
         </div>
-        {scrollPosition < 100 && (
-          <a href="#about" className="absolute bottom-0 animate-bounce">
-            <img
-              src="down.svg"
-              alt="Scroll Down for more!"
-            />
-          </a>
-        )}
       </div>
     </div>
   );
